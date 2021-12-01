466ba85b17a476cf023e6de57e175fad4ebb7f1b
楓太 小山 <190138@jc-21.jp>
2021-12-01 14:18:10 +0900

    肥満

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,57 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            String heigth = heigth.Text;
+            String weigth = weigth.Text;
+            try
+            {
+                double h = double.Parse(heigth);
+                double w = double.Parse(weigth);
+                string zyoutai;
 
+                if (h > 10)
+                {
+                    h = h / 100;
+                }
+                if (w > 10000)
+                {
+                    w = w / 1000;
+                }
+                double BMI = Math.Round(w / (h * h), 1);
+
+                if (BMI < 18.5)
+                {
+                    zyoutai = "低体重（痩せ）";
+
+                }
+                else if (BMI < 25.0)
+                {
+                    zyoutai = "普通体重";
+                }
+                else if (BMI < 30.0)
+                {
+                    zyoutai = "肥満度(1度)";
+                }
+                else if (BMI < 35.0)
+                {
+                    zyoutai = "肥満度(2度)";
+                }
+                else if (BMI < 40.0)
+                {
+                    zyoutai = "肥満度(3度)";
+                }
+                else
+                {
+                    zyoutai = "肥満度(4度)";
+                }
+                result.Text = "BMIは" + BMI + "で、体形は" + zyoutai + "す。";
+            }catch(FormatException ex)
+            {
+                Console.WriteLine(ex);
+                result.Text = "!!!";
+            }
+
+            }
         }
     }
-}
+
